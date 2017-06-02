4f1734cdeb0a327ecd600a50dc0892a8248b9dd9
Tony Proum <t.proum@gmail.com>
2017-06-02 13:44:34 +0200

    update docker-compose version

--- install.sh
+++ install.sh
@@ -36,7 +36,7 @@ apt-get update
 apt-get -y install docker-engine
 
 # Install docker compose
-curl -L https://github.com/docker/compose/releases/download/1.9.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.13.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
 # run rancher stack
